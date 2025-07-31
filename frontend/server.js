748c6704b3f00f5bf54ef715083a88937ff398e3
Tanmay-Kanase <tanmaykanase06@gmail.com>
2025-07-31 22:49:28 +0530

    .

---
+++ frontend/server.js
@@ -0,0 +1,14 @@
+import path from "path";
+import express from "express";
+const __dirname = path.resolve();
+
+const app = express();
+
+app.listen(5173, () => {
+  console.log("server is running on port 5173!!!!!");
+});
+
+app.use(express.static(path.join(__dirname, "/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
